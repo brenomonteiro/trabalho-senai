995c3e7f85c9a44503a654f96fb0dcf11bf92663
Apollo C. <parvelotth.larka@gmail.com>
2025-04-27 00:39:01 -0300

    Add files via upload

---
+++ Idade idosa/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void idosa (void)
+{
+    int idade, anoatual, anonascimento;
+
+    printf("Ano atual: ");
+    scanf("%d", &anoatual);
+    printf("Ano de nascimento: ");
+    scanf("%d", &anonascimento);
+    printf("Idade do individuo: %d\n", idade = anoatual - anonascimento);
+    if (idade >= 51)
+    {
+        printf("O individuo e idoso\n");
+    }
+    else if (idade );
+}
+
+int main ()
+{
+    idosa ();
+    return 0;
+}
